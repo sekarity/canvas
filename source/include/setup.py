c77ff6be85ecbb347c8b5997115cbc0d6ce84934
Sekar <sekarkulandaivel@gmail.com>
2019-08-14 13:11:36 -0700

    Create setup.py

---
+++ source/include/setup.py
@@ -0,0 +1,12 @@
+import bisect, fractions, math, os, pickle, statistics, sys, time
+
+# for all files
+TIMING = True
+STATS = True
+
+# for extractor
+FORMAT = 'candump'
+
+# for classifier
+STRONG_THRESHOLD = 0.06
+DISCONTINUOUS_THRESHOLD = 4
